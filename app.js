2bab80bbdc48d06c6e7bfd9cad66f1fa5048e3dd
Beloved Bags <beloved.bags.columbus@gmail.com>
2020-10-31 16:29:39 -0400

    add start script

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ app.get('/', function(req, res){
 })
 
 app.listen(process.env.PORT || 3000, function (err){
-    console.log('server started on port 3000');
+    console.log('server started");
 
 })
