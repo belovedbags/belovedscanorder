12a481c61f95b2fa5bc521daa995653fc374fde9
beloved bags <beloved.bags.columbus@gmail.com>
2020-10-31 16:06:15 -0400

    Init

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+
+var bodyParser = require('body-parser');
+var methodOverride = require("method-override"); 
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.use(methodOverride("_method"));
+//=====================
+//      ROUTES
+//=====================
+
+app.get('/', function(req, res){
+
+    res.render("home");
+
+})
+
+app.listen(process.env.PORT || 3000, function (err){
+    console.log('server started on port 3000');
+
+})
